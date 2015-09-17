9fafac8c1d42e87666334d1c0b4db0dc94f25a1e
Brian P. Holt <bholt@dwolla.com>
2015-09-17 14:17:14 -0500

    fix overzealous s/networking/network/

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ DIR=`dirname "$0"`
 
 cp "$DIR/etc/init.d/fake-ec2-metadata-service" /etc/init.d/fake-ec2-metadata-service
 cp "$DIR/etc/network/interfaces.d/fake-ec2-metadata-service" /etc/network/interfaces.d/
-/etc/init.d/network restart
+/etc/init.d/networking restart
 ifup -a
 
 if ! ifconfig | fgrep 169.254.169.254 > /dev/null 2>&1; then
