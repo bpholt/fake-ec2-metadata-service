93245d523fa36e8641b155b19bea3c74f93c032a
Brian P. Holt <bholt@dwolla.com>
2015-09-17 14:11:48 -0500

    add sysv init script and setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+DIR=`dirname "$0"`
+
+cp "$DIR/etc/init.d/fake-ec2-metadata-service" /etc/init.d/fake-ec2-metadata-service
+cp "$DIR/etc/network/interfaces.d/fake-ec2-metadata-service" /etc/network/interfaces.d/
+/etc/init.d/network restart
+ifup -a
+
+if ! ifconfig | fgrep 169.254.169.254 > /dev/null 2>&1; then
+    echo "Network setup failed; loopback interface on 169.254.169.254 not available."
+    exit 1
+fi
