adc80de0b5c73a3ad4a781b5f43dea8912362c32
Brian P. Holt <bholt@dwolla.com>
2015-09-24 18:27:35 -0500

    specifically exclude the Docker interface, which runs on 172.17.42.1
    see reference: https://docs.docker.com/articles/networking/

--- ec2-metadata-service.rb
+++ ec2-metadata-service.rb
@@ -8,7 +8,7 @@ set :bind, '169.254.169.254'
 set :port, '80'
 
 get '/latest/meta-data/local-ipv4' do
-    ipv4 = (Socket.ip_address_list.select {|a| a.ipv4_private? }).last
+    ipv4 = (Socket.ip_address_list.select {|a| a.ipv4_private? && a.ip_address != "172.17.42.1" }).last
     ipv4.ip_address
 end
 
