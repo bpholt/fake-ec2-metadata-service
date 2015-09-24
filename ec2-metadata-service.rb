2111eeca1f9cbbadb0027f1bfc353fce2e4bc061
Brian P. Holt <bholt@dwolla.com>
2015-09-24 18:38:39 -0500

    instead of just excluding 172.17.42.1, exclude all of 172.17.42.1/16

--- ec2-metadata-service.rb
+++ ec2-metadata-service.rb
@@ -3,12 +3,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'socket'
+require 'ipaddr'
 
 set :bind, '169.254.169.254'
 set :port, '80'
 
+docker_range = IPAddr.new("172.17.42.1/16")
+
 get '/latest/meta-data/local-ipv4' do
-    ipv4 = (Socket.ip_address_list.select {|a| a.ipv4_private? && a.ip_address != "172.17.42.1" }).last
+    ipv4 = (Socket.ip_address_list.select {|a| a.ipv4_private? && !(docker_range === a.ip_address) }).last
     ipv4.ip_address
 end
 
