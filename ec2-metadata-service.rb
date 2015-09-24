748b39a76bfde56769bc8f3270725541fab39bf5
Brian P. Holt <bholt@dwolla.com>
2015-09-24 16:02:22 -0500

    if multiple private ip-having interfaces are found, use the last one to work around virtualbox and vagrant being dumb

--- ec2-metadata-service.rb
+++ ec2-metadata-service.rb
@@ -8,7 +8,7 @@ set :bind, '169.254.169.254'
 set :port, '80'
 
 get '/latest/meta-data/local-ipv4' do
-    ipv4 = Socket.ip_address_list.find {|a| a.ipv4_private? }
+    ipv4 = (Socket.ip_address_list.select {|a| a.ipv4_private? }).last
     ipv4.ip_address
 end
 
